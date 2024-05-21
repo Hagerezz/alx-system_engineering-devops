ebcd57ea06a2fbbfa9f59335ccd99987690f05af
hagerezz <queen.emilly12@gmail.com>
2024-05-21 03:10:40 +0300

    h

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Define a Puppet manifest to create a file in /tmp with specified attributes
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
