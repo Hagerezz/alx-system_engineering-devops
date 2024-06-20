67ccf4e38c00f5490cc2f5708b7d2cdf7872c18e
hagerezz <queen.emilly12@gmail.com>
2024-06-20 04:00:07 +0300

    hi

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,10 +3,10 @@
 
 file { '/etc/ssh/ssh_config':
   ensure  => present,
-content =>"
-
+content => "
     # SSH client configuration
-    host*
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
 }
