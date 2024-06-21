d0d55fae594e5ee49f4e74b69af8ef572b8fdb5a
hagerezz <queen.emilly12@gmail.com>
2024-06-21 04:52:15 +0300

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False)
+    response = requests.get(url)
     if response.status_code / 100 == 2:
         data = response.json()
         subscribers = data['data']['subscribers']
